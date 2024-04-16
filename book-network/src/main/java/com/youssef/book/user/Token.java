0ab570e9ad5d8dd09166d227fbc3b7b43b9f2a95
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-16 09:24:16 +0100

    created Token Entity

---
+++ book-network/src/main/java/com/youssef/book/user/Token.java
@@ -0,0 +1,27 @@
+package com.youssef.book.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+    @Id @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
