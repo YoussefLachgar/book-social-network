cdeaebc6f0535a42cbc7d09d3dbd6865aad2a4c1
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-18 21:06:35 +0100

    updating JwtFilter, by implementing doFilterInternal, and creating jweService

--- book-network/src/main/java/com/youssef/book/security/JwtFilter.java
+++ book-network/src/main/java/com/youssef/book/security/JwtFilter.java
@@ -1,7 +1,42 @@
 package com.youssef.book.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Service
-public class JwtFilter {
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        if (request.getServletPath().contains("/api/v1/auth")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String authHeader = request.getHeader(AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+
+    }
 }
---
+++ book-network/src/main/java/com/youssef/book/security/JwtService.java
@@ -0,0 +1,9 @@
+package com.youssef.book.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String jwt) {
+    }
+}
