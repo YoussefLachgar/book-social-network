954fcf4cbd8595cbfaadd7659a259e46b2cc2019
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-18 23:01:14 +0100

    setuping some function in JwtService class

--- book-network/src/main/java/com/youssef/book/security/JwtService.java
+++ book-network/src/main/java/com/youssef/book/security/JwtService.java
@@ -1,9 +1,48 @@
 package com.youssef.book.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
+    private long jwtExpiration;
+    private String secretKey;
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> claims, UserDetails userDetails){
+        return buildToken(claims, userDetails, jwtExpiration);
+    }
+
+    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long jwtExpiration){
+        var authorities = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+        return Jwts.builder().setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .claim("authorities", authorities)
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
     public String extractUsername(String jwt) {
+        return "";
     }
 }
