399d54cef5af89460c7ae4dd42e315a8c220e4b2
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-19 15:26:51 +0100

    updating AuthenticationService, add generateActivationCode function they generate the code to verify the email

--- book-network/src/main/java/com/youssef/book/auth/AuthenticationService.java
+++ book-network/src/main/java/com/youssef/book/auth/AuthenticationService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
 import java.util.List;
 
 @Service
@@ -45,7 +46,16 @@ public class AuthenticationService {
         return null;
     }
 
-
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; i++){
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
+    }
 
 
 }
