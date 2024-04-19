747e4e5860438eef2408b602cea9ceb933442e01
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-19 11:57:41 +0100

    updating AuthenticationController, add register function

--- book-network/src/main/java/com/youssef/book/auth/AuthenticationController.java
+++ book-network/src/main/java/com/youssef/book/auth/AuthenticationController.java
@@ -1,9 +1,11 @@
 package com.youssef.book.auth;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
@@ -12,5 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request){
+        authenticationService.register(request);
+        return ResponseEntity.accepted().build();
+    }
+
 
 }
