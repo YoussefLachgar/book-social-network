759ad36e58946cfaffc0ae39a68a0b7081021a75
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-20 15:26:23 +0100

    creating EmailTemplateName enum, and add sendEmail function to EmailService

--- book-network/src/main/java/com/youssef/book/email/EmailService.java
+++ book-network/src/main/java/com/youssef/book/email/EmailService.java
@@ -1,4 +1,17 @@
 package com.youssef.book.email;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+@Service
+@RequiredArgsConstructor
 public class EmailService {
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    public void sendEmail(String to, String username, EmailTemplateName emailTemplate, String confirmation, String activation, String subject){
+
+    }
 }
---
+++ book-network/src/main/java/com/youssef/book/email/EmailTemplateName.java
@@ -0,0 +1,4 @@
+package com.youssef.book.email;
+
+public enum EmailTemplateName {
+}
