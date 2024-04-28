4829789d1598d16d05eed5a828a2ba1ba2a21261
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-28 12:35:23 +0100

    updating application-dev, pom.xml . refactoring the code to be working with Mysql databse localy

--- book-network/src/main/java/com/youssef/book/Application.java
+++ book-network/src/main/java/com/youssef/book/Application.java
@@ -1,7 +1,11 @@
 package com.youssef.book;
 
+import com.youssef.book.role.Role;
+import com.youssef.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,6 +16,17 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository){
+		return  args -> {
+			if (roleRepository.findByName("USER").isEmpty()){
+				roleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
 	}
 
 }
--- book-network/src/main/java/com/youssef/book/role/Role.java
+++ book-network/src/main/java/com/youssef/book/role/Role.java
@@ -3,10 +3,7 @@ package com.youssef.book.role;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.youssef.book.user.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -19,6 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 @EntityListeners(AuditingEntityListener.class)
 public class Role {
     @Id @GeneratedValue
