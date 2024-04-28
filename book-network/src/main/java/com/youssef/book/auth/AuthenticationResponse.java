7f7b1aaf9cef298d648dccc2db30a0e59682f76f
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-28 12:52:10 +0100

    Updating AuthenticatinService, by create autheticate function

--- book-network/src/main/java/com/youssef/book/auth/AuthenticationResponse.java
+++ book-network/src/main/java/com/youssef/book/auth/AuthenticationResponse.java
@@ -1,4 +1,13 @@
 package com.youssef.book.auth;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class AuthenticationResponse {
+
+    private String token;
 }
--- book-network/src/main/java/com/youssef/book/auth/AuthenticationService.java
+++ book-network/src/main/java/com/youssef/book/auth/AuthenticationService.java
@@ -85,4 +85,6 @@ public class AuthenticationService {
     }
 
 
+    public Object authenticate(AuthenticationRequest request) {
+    }
 }
